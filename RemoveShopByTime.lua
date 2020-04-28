b922fdb7bf3fcc420bc6b399e51621db0e47ae22
Philipp Jäger <46376130+nilBrain@users.noreply.github.com>
2020-04-28 17:48:12 +0200

    Add files via upload
    Revision of the L10N and Time revision in the script

--- RemoveShopByTime.lua
+++ RemoveShopByTime.lua
@@ -8,7 +8,7 @@
 RemoveShopByTime={}
 
 function RemoveShopByTime:update(dt)
-	if g_currentMission.environment.currentHour >= 18 and g_currentMission.environment.currentHour < 5 then
+	if g_currentMission.environment.currentHour == 12 or g_currentMission.environment.isSunOn then
 		if g_gui.currentGui == g_gui.guis.ShopMenu then
 			g_gui:showGui("");
 			g_gui:showInfoDialog({visible = true, text = g_i18n:getText("shop_shopIsClosed"), dialogType = DialogElement.TYPE_INFO, isCloseAllowed = true});
